52dcd04755611f36cc3bf8b0a4ec11198fd8a307
Yash Kaskar <yash.kaskar99@gmail.com>
2023-02-22 15:56:39 +0530

    change

---
+++ src/Components/Products/Product/Product.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Card, CardActions, CardContent, CardMedia, IconButton, Typography } from '@material-ui/core'
+import { AddShoppingCart } from '@material-ui/icons'
+
+import useStyles from './style'
+
+const Product = ({ product }) => {
+    const classes = useStyles();
+    
+  return (
+    <Card className={classes.root}>  
+        <CardMedia className={classes.media} image=''  />
+        <CardContent>
+            <div className={classes.cardcontainer}>
+                <Typography variant='h5' gutterBottom>
+                      {product.name}
+                </Typography>
+                <Typography variant='h5'>
+                      {product.price}
+                </Typography>     
+            </div>   
+        <Typography variant='h2' color='text-Secondary'>
+                      {product.description}
+        </Typography>  
+        </CardContent>  
+        <CardActions disableSpacing className={classes.cardActions}>
+            <IconButton aria-label='Add to Cart'>
+                <AddShoppingCart />  
+            </IconButton> 
+        </CardActions >  
+    </Card>
+  )
+}
+
+export default Product
