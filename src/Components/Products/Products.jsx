c473e2c7596dc4258a91f5d927fe03501ff2861c
Yash Kaskar <yash.kaskar99@gmail.com>
2023-02-23 17:02:50 +0530

    product

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -14,7 +14,7 @@ const Products = () => {
     <Grid container justifyContent='center' spacing={4}>
         {products.map((product) => (    
             <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                <Product />
+                <Product product= {product} />
             </Grid>
         ))}      
     </Grid>    
