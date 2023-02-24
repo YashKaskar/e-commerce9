778f0448d9706725fb876dac04ab3deaf7383aa4
Yash Kaskar <yash.kaskar99@gmail.com>
2023-02-24 14:12:51 +0530

    height of logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
        <Toolbar>
         {/* left side */}
          <Typography variant='h6' className= {classes.title} color= 'inherit'>   
-            <img src= {logo} alt="Open" height='30px' className= {classes.image} />
+            <img src= {logo} alt="Open" height='60px' className= {classes.image} />
              Open
          </Typography>
          <div className= {classes.grow} />
