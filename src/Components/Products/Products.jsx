0422c2f97e986f7861a963663ffa842a08a7b7e9
Yash Kaskar <yash.kaskar99@gmail.com>
2023-02-27 12:17:24 +0530

    adding product

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -5,13 +5,10 @@ import Product from './Product/Product'
 
 import useStyles from './style'
 
-const products = [
-    { id: 1, name:'Puma', price: '$2', image : 'https://images.unsplash.com/photo-1605034313761-73ea4a0cfbf3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80', description: 'Sneakers' },
-    { id: 2, name:'B-twin', price: '$20', image : 'https://images.unsplash.com/photo-1532298229144-0ec0c57515c7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1122&q=80', description: 'Cycle'}
-]
 
-const Products = () => {
+const Products = ({products}) => {
   const classes = useStyles()
+  
   return (
     <main className={classes.content}>  
       <div className={ classes.toolbar} />
