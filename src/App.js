adaa271620126651227c398df835746877b2dbcf
Yash Kaskar <yash.kaskar99@gmail.com>
2023-02-28 12:08:55 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,28 @@
 import React, { useEffect, useState } from 'react'
-import {Products, Navbar, Cart} from './Components'
+import {Products, Navbar} from './Components'
 import { commerce } from './libraries/commerce';
 
 
 const App = () => {
 
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState({});
 
   const fetchProducts = async () => { 
     const { data } = await commerce.products.list();
     setProducts(data);
   }
 
-  const fetchCart = async () => {
-      setCart(await commerce.cart.retrieve());
-  }
-
-  const handleAddToCart = async (productId, quantity) => { 
-    const item = await commerce.cart.add(productId, quantity)
-    setCart(item.cart)
-  }
-
 
   useEffect(() => {  
-    fetchProducts();
-    fetchCart()
+    fetchProducts()
   }, [])
   
-  
+
   return (
     <div>
-      < Navbar totalItems={cart.total_items} />
-      {/* < Products products = {products} onAddToCart ={handleAddToCart} /> */}
-      < Cart cart={cart} />
+      < Navbar />
+      < Products products = {products} />
+     
     </div>
   )
 }
