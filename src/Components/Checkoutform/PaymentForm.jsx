91e88424e742b0e21d1e6ec68f351ed29db46e57
Yash Kaskar <yash.kaskar99@gmail.com>
2023-03-13 12:18:44 +0530

    changes

--- src/Components/Checkoutform/PaymentForm.jsx
+++ src/Components/Checkoutform/PaymentForm.jsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import { Button, Divider, Typography } from '@material-ui/core'
-import { CardElement, Elements, ElementsConsumer } from '@stripe/react-stripe-js'
-import Review from './Review'
+import React from 'react';
+import { Typography, Button, Divider } from '@material-ui/core';
+import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY)
+import Review from './Review';
 
-const PaymentForm = ({ checkoutToken, shippingData, nextStep, backStep, onCaptureCheckout, timeout }) => {
-  const handleSubmit = async (event, elements, stripe) => {  
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+
+const PaymentForm = ({ checkoutToken, nextStep, backStep, shippingData, onCaptureCheckout }) => {
+  const handleSubmit = async (event, elements, stripe) => {
     event.preventDefault();
 
-    if (!stripe || !elements) return
-    
-    const cardElement = elements.getElement(CardElement)
+    if (!stripe || !elements) return;
+
+    const cardElement = elements.getElement(CardElement);
 
     const { error, paymentMethod } = await stripe.createPaymentMethod({ type: 'card', card: cardElement });
 
-    if(error) {
-      console.log(error);
-    } else { 
+    if (error) {
+      console.log('[error]', error);
+    } else {
       const orderData = {
-        line_items: checkoutToken.line_items,
-        customer: { firstName: shippingData.firstName, lastName: shippingData.lastName, email: shippingData.email },
+        line_items: checkoutToken.live.line_items,
+        customer: { firstname: shippingData.firstName, lastname: shippingData.lastName, email: shippingData.email },
         shipping: { name: 'International', street: shippingData.address1, town_city: shippingData.city, county_state: shippingData.shippingSubdivision, postal_zip_code: shippingData.zip, country: shippingData.shippingCountry },
         fulfillment: { shipping_method: shippingData.shippingOption },
         payment: {
@@ -33,11 +34,10 @@ const PaymentForm = ({ checkoutToken, shippingData, nextStep, backStep, onCaptur
       };
 
       onCaptureCheckout(checkoutToken.id, orderData);
-      timeout();
+
       nextStep();
     }
-  }
-
+  };
   return (
     <>
       <Review checkoutToken={checkoutToken} />
